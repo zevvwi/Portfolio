57d34880a403e45e6ae9253632f4100e0e7ca349
zeywi <zeywi@mail.ru>
2022-11-12 21:41:28 +0700

    Create resume screen

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
 let hamburger = document.querySelector('.hamburger'),
     close = document.querySelector('.menu__close'),
     menu = document.querySelector('.menu');
+    window = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener("click", () => {
     menu.classList.toggle('active');
