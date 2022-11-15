13fa382214aec3f332fab8670353b34619615d27
zeywi <zeywi@mail.ru>
2022-11-16 00:59:38 +0700

    Add validation and php

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,80 @@
-let hamburger = document.querySelector('.hamburger'),
-    close = document.querySelector('.menu__close'),
-    menu = document.querySelector('.menu');
-    window = document.querySelector('.menu__overlay');
+$(document).ready(function(){
+    let hamburger = document.querySelector('.hamburger'),
+        close = document.querySelector('.menu__close'),
+        menu = document.querySelector('.menu');
+        window = document.querySelector('.menu__overlay');
+        linkClose = document.querySelectorAll('.menu__link')
+        counter = document.querySelectorAll('.checkpoint__percentage');
+        line = document.querySelectorAll('.checkpoint__line-main .checkpoint__line-second');
 
-hamburger.addEventListener("click", () => {
-    menu.classList.toggle('active');
-});
+    hamburger.addEventListener("click", () => {
+        menu.classList.toggle('active');
+    });
+    close.addEventListener("click", () => {
+        menu.classList.toggle('active');
+    });
+    linkClose.forEach((item, i) => {
+        linkClose[i].addEventListener("click", () => {
+            menu.classList.remove('active');
+        });
+    });
+    counter.forEach((item, i) => {
+        line[i].style.width = item.innerHTML;
+    });
 
-close.addEventListener("click", () => {
-    menu.classList.toggle('active');
-});
 
-const counter = document.querySelectorAll('.checkpoint__percentage');
-      line = document.querySelectorAll('.checkpoint__line-main .checkpoint__line-second');
-
-counter.forEach( (item, i) => {
-    line[i].style.width = item.innerHTML;
-});
+    function validateForm(form) {
+        $(form).validate({
+          rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true,
+                minlength: 3
+            },
+            privacy: {
+                required: true
+            },
+            text: {
+                required: true
+            }
+          },
+          messages: {
+            name: {
+              required: "Введите свое имя",
+              minlength: jQuery.validator.format("Минимальное кол-во символов: {0}")
+            },
+            email: {
+              required: "Введите свой Email адрес",
+              email: "Введите корректный Email адрес",
+              minlength: jQuery.validator.format("Минимальное кол-во символов: {0}")
+            },
+            privacy: {
+                required: "Подтвердите согласие с политикой конфиденциальности"
+            },
+            text: {
+                required: "Введите сообщение",
+            }
+          }
+        });
+      }
+      $("#name").blur(); 
+  
+      validateForm('#contacts__form');
+  
+      $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: "../mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function() {
+          $(this).find("input").val("");
+          $('form').trigger('reset');
+        });
+        return false;
+      });
+})
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -1,8 +1,8 @@
 <?php 
 
 $name = $_POST['name'];
-$phone = $_POST['phone'];
 $email = $_POST['email'];
+$text = $_POST['text'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
@@ -11,16 +11,16 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'zeywi@mail.ru';                 // Наш логин
-$mail->Password = 'uwubnk5BB03rTJvwxs8U';                           // Наш пароль от ящика
+$mail->Username = 'hiroto.development@gmail.com';                 // Наш логин
+$mail->Password = 'nY8Ositucv';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('zeywi@mail.ru', 'RunSmart');   // От кого письмо 
-$mail->addAddress('mihailbarsukov121207@gmail.com');     // Add a recipient
-$mail->addAddress('Korotkovgk17@yandex.ru');               // Name is optional
+$mail->setFrom('HirotoDev');   // От кого письмо 
+$mail->addAddress('hiroto.development@gmail.com');     // Add a recipient
+// $mail->addAddress('Korotkovgk17@yandex.ru');               // Name is optional
 // $mail->addReplyTo('Korotkovgk17@yandex.ru');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
@@ -32,8 +32,8 @@ $mail->Subject = 'Данные';
 $mail->Body    = '
 		Пользователь оставил данные <br> 
 	Имя: ' . $name . ' <br>
-	Номер телефона: ' . $phone . '<br>
-	E-mail: ' . $email . '';
+	E-mail: ' . $email . ' <br>
+	Сообщение: ' . $text . ''
 
 if(!$mail->send()) {
     return false;
